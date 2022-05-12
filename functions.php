9d619d292719f3f9515b0485262693d769c3f725
skebadur <muhammad.ebadur@gmail.com>
2022-05-12 11:47:41 +0600

    test

---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+
+
+add_action('acf/init', 'my_acf_blocks_init');
+function my_acf_blocks_init() {
+
+    // Check function exists.
+    if( function_exists('acf_register_block_type') ) {
+
+        // Register a testimonial block.
+        acf_register_block_type(array(
+            'name'              => 'testimonial',
+            'title'             => __('Testimonial'),
+            'description'       => __('A custom testimonial block.'),
+            'render_template'   => 'template-parts/blocks/testimonial/testimonial.php',
+            'category'          => 'formatting',
+        ));
+    }
+}
